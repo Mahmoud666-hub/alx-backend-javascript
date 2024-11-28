ec5faf782e70e188c6143d68f7f5f2d0ae60e724
root <root@DESKTOP-SVJM9K1>
2024-11-28 17:44:59 +0200

    mr

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-console.log('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
   const nam = process.stdin.read();
@@ -8,5 +8,5 @@ process.stdin.on('readable', () => {
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
